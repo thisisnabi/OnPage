2e398878e0d5a1ec01fc3b069ddead47a8e24afb
N.Karampour <N.Karampour@alibaba.ir>
2023-07-24 16:26:34 +0300

    Add project files.

---
+++ IQueryableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.EntityFrameworkCore;
+
+public static class IQueryableExtensions
+{
+    public static IQueryable<T> Page<T>(this IQueryable<T> query, int pageIndex, int pageSize)
+        => query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+    public static IQueryable<T> FirstPage<T>(this IQueryable<T> query, int pageSize)
+        => query.Take(pageSize);
+
+    public static IQueryable<T> LastPage<T>(this IQueryable<T> query, int pageSize)
+        => query.TakeLast(pageSize);
+
+    public static int CountOfPage<T>(this IQueryable<T> query, int pageSize)
+    { 
+        var total = query.Count();
+        return (total / pageSize) + ((total % pageSize) > 0 ? 1 : 0);
+    }
+}
