841ba8b77d752fd0820c06cddc479818f7410022
N.Karampour <N.Karampour@alibaba.ir>
2023-07-24 20:09:41 +0300

    Change CountOfPages method name

--- IQueryableExtensions.cs
+++ IQueryableExtensions.cs
@@ -11,7 +11,7 @@ public static class IQueryableExtensions
     public static IQueryable<T> LastPage<T>(this IQueryable<T> query, int pageSize)
         => query.TakeLast(pageSize);
 
-    public static int CountOfPage<T>(this IQueryable<T> query, int pageSize)
+    public static int CountOfPages<T>(this IQueryable<T> query, int pageSize)
     { 
         var total = query.Count();
         return (total / pageSize) + ((total % pageSize) > 0 ? 1 : 0);
