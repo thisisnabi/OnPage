d9f768c6c5656a568f48930518a278a085c211e6
thisisnabi <thisisnabi@outlook.com>
2023-08-20 18:01:10 +0330

    feat: Add PageResult for geeting IQuerayble result
    #5

---
+++ src/PagedResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+namespace Microsoft.EntityFrameworkCore.Pagination;
+
+public class PagedResult<T>
+{
+    public PagedResult(IQueryable<T> query, int pageIndex, int pageSize, long totalItemsCount)
+    {
+        if (query == null)
+            throw new ArgumentNullException(nameof(query));
+        Items = query.Page(pageIndex, pageSize).ToImmutableArray();
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        TotalItemsCount = totalItemsCount;
+        TotalPagesCount = pageSize != 0L ? (long)Math.Ceiling(totalItemsCount / (Decimal)pageSize) : 0L;
+    }
+
+    public long PageIndex { get; }
+
+    public long PageSize { get; }
+
+    public long TotalPagesCount { get; }
+
+    public long TotalItemsCount { get; }
+
+    public IReadOnlyCollection<T> Items { get; }
+
+}
+
+public static class PagedResultExtensions
+{
+    public static PagedResult<T> ToPageResult<T>(this IQueryable<T> query, int pageIndex, int pageSize)
+        => new PagedResult<T>(query, pageIndex, pageSize, query.Count());
+}
+
